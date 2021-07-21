25013874392bcbc366307a4e44b51681e6fcc9e8
Aabhash Thapa <87767651+aabhash07@users.noreply.github.com>
2021-07-21 22:10:19 +0545

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include<conio.h>
+void main()
+{
+    int a,b,s;
+    printf("enter numbers:\n");
+    scanf("%d%d",&a,&b);
+    //printf("enter another number:\n");
+    //scanf("%d",&b);
+    s=a+b;
+    printf("the sum is:%d",s);
+}
